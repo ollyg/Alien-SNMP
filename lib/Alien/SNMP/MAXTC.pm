a29de2991ff107c8a5128ebbd9e9116e2678ebc9
Oliver Gorwits <oliver@cpan.org>
2017-08-12 15:29:21 +0100

    release 1.000000

--- lib/Alien/SNMP/MAXTC.pm
+++ lib/Alien/SNMP/MAXTC.pm
@@ -5,7 +5,7 @@ use warnings;
 use 5.010001;
 use parent qw(Alien::Base);
 
-our $VERSION = '0.02';
+our $VERSION = '1.000000';
 
 1;
 
@@ -21,7 +21,7 @@ Alien::SNMP::MAXTC - Alien package for the Net-SNMP library
 
 =head1 VERSION
 
-Version 0.02
+Version 1.000000
 
 =cut
 
